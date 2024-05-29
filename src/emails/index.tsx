30f938949f6feb4132fb54ecb1b8e5f57cb001bb
Aelpxy <aelpxy@protonmail.com>
2024-05-29 23:59:21 +0600

    feat: add `react-email` and `nodemailer`

---
+++ src/emails/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Html, Body } from '@react-email/components';
+
+export default function Email() {
+    return (
+        <Html>
+            <Body>Hello from Kepler!</Body>
+        </Html>
+    );
+}
