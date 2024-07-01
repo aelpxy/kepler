fd8b7db395a40434797f0bd1291e7f747346608e
Aelpxy <aelpxy@protonmail.com>
2024-07-02 00:30:32 +0600

    chore: bump deps

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,17 +1,17 @@
-import { pgTable, text, uniqueIndex } from "drizzle-orm/pg-core";
+import { pgTable, text, uniqueIndex } from 'drizzle-orm/pg-core';
 
 export const users = pgTable(
-  "users",
-  {
-    id: text("id").notNull().unique().primaryKey(),
-    avatarPath: text("avatar_path").notNull().unique(),
-    username: text("username").notNull().unique(),
-    email: text("email").notNull().unique(),
-  },
-  (table) => {
-    return {
-      usernameIdx: uniqueIndex("username_idx").on(table.username),
-      emailIdx: uniqueIndex("email_idx").on(table.email),
-    };
-  }
+    'users',
+    {
+        id: text('id').notNull().unique().primaryKey(),
+        avatarPath: text('avatar_path').notNull().unique(),
+        username: text('username').notNull().unique(),
+        email: text('email').notNull().unique(),
+    },
+    (table) => {
+        return {
+            usernameIdx: uniqueIndex('username_idx').on(table.username),
+            emailIdx: uniqueIndex('email_idx').on(table.email),
+        };
+    }
 );
