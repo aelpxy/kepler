487cde86b025890c0b8cdf43f428e8f6fc49f0d8
Aelpxy <aelpxy@protonmail.com>
2024-07-04 19:00:17 +0600

    perf: migrate to `fast-jwt`

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,61 +1,28 @@
-import * as JsonWebToken from 'jsonwebtoken';
-import { appConfig } from '@/config/app.config';
-
-interface JwtPayload {
-    userId: string;
-    role: string;
-}
+import { createSigner, createVerifier } from "fast-jwt";
+import { appConfig } from "@/config/app.config";
 
 class Jwt {
-    private static secretKey = appConfig.JWT_SECRET as string;
-    private static jwtIssuer = appConfig.JWT_ISSUER;
+  private static secretKey = appConfig.JWT_SECRET as string;
+  private static jwtIssuer = appConfig.JWT_ISSUER;
 
-    public async generateJwtToken(
-        payload: JwtPayload,
-        expiresIn: string
-    ): Promise<string> {
-        return new Promise<string>((resolve, reject) => {
-            JsonWebToken.sign(
-                payload,
-                Jwt.secretKey,
-                { expiresIn: expiresIn, issuer: Jwt.jwtIssuer },
-                (error: Error | null, token?: string) => {
-                    if (error) {
-                        reject(new Error(error.message));
-                    } else {
-                        if (token) {
-                            resolve(token);
-                        } else {
-                            reject(new Error('Token could not be generated'));
-                        }
-                    }
-                }
-            );
-        });
-    }
+  async sign(payload: Object, expiresIn: number) {
+    const signer = createSigner({
+      key: Jwt.secretKey,
+      iss: Jwt.jwtIssuer,
+      expiresIn,
+    });
 
-    public async verifyJwtToken(token: string): Promise<JwtPayload> {
-        return new Promise<JwtPayload>((resolve, reject) => {
-            JsonWebToken.verify(
-                token,
-                Jwt.secretKey,
-                {
-                    issuer: Jwt.jwtIssuer,
-                },
-                (error: Error | null, decoded?: object | string) => {
-                    if (error) {
-                        reject(new Error(error.message));
-                    } else {
-                        if (typeof decoded === 'string') {
-                            reject(new Error('Token could not be verified'));
-                        } else {
-                            resolve(decoded as JwtPayload);
-                        }
-                    }
-                }
-            );
-        });
-    }
+    return signer(payload);
+  }
+
+  async decode(token: string) {
+    const decoder = createVerifier({
+      key: Jwt.secretKey,
+      allowedIss: Jwt.jwtIssuer,
+    });
+
+    return await decoder(token);
+  }
 }
 
 const jwt = new Jwt();
